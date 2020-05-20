7c41967ebe9742ca9c8d7d79f0f133d84828e99e
Doxygen CI <travis@travis-ci.org>
2020-05-20 19:05:26 +0000

    Deploy code docs to GitHub Pages Travis build: 14
    Commit: efc7104187c7795205a02d104b96547cec5be34b

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -3,6 +3,5 @@ var searchData=
   ['adafruit_5fmax31856',['Adafruit_MAX31856',['../class_adafruit___m_a_x31856.html',1,'Adafruit_MAX31856'],['../class_adafruit___m_a_x31856.html#a14a4881b026f6dc555da759abcb0d9d1',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31856.html#a58c30d7e5be4257cd17f86aaccde80a9',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs)']]],
   ['adafruit_5fmax31856_2ecpp',['Adafruit_MAX31856.cpp',['../_adafruit___m_a_x31856_8cpp.html',1,'']]],
   ['adafruit_5fmax31856_2eh',['Adafruit_MAX31856.h',['../_adafruit___m_a_x31856_8h.html',1,'']]],
-  ['adafruit_20max31856_20thermocouple_20reader',['Adafruit MAX31856 thermocouple reader',['../index.html',1,'']]],
-  ['adafruit_20max31856_20arduino_20library_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmax31856_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmax31856_29',['Adafruit MAX31856 Arduino Library  [![Build Status](https://travis-ci.org/adafruit/Adafruit_MAX31856.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MAX31856)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20max31856_20thermocouple_20reader',['Adafruit MAX31856 thermocouple reader',['../index.html',1,'']]]
 ];
--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['adafruit_20max31856_20thermocouple_20reader',['Adafruit MAX31856 thermocouple reader',['../index.html',1,'']]],
-  ['adafruit_20max31856_20arduino_20library_20_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmax31856_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fmax31856_29',['Adafruit MAX31856 Arduino Library  [![Build Status](https://travis-ci.org/adafruit/Adafruit_MAX31856.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_MAX31856)',['../md__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20max31856_20thermocouple_20reader',['Adafruit MAX31856 thermocouple reader',['../index.html',1,'']]]
 ];
