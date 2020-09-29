c8731a9ca68569cd5e5f8795b7a32dd48e21903c
Doxygen CI <travis@travis-ci.org>
2020-09-29 20:59:13 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_adafruit___m_a_x31856.html#a367711ffbc2b751c07f6b640c951ce41',1,'Adafruit_MAX31856']]]
+  ['begin',['begin',['../class_adafruit___m_a_x31856.html#af58bc03e79eb53d106402cb09e374f1f',1,'Adafruit_MAX31856']]]
 ];
