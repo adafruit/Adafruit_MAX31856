239bf0e2cf7cc80ff6b8c61675903843dd1d3654
Doxygen CI <ci-arduino@invalid>
2021-04-07 17:23:17 +0000

    Deploy docs to GitHub Pages from commit 299626c393
    Commit: 299626c3935c1f32851820b1063889342eda10ec
    GitHub Actions run: 726770922

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['adafruit_5fmax31856',['Adafruit_MAX31856',['../class_adafruit___m_a_x31856.html',1,'Adafruit_MAX31856'],['../class_adafruit___m_a_x31856.html#a14a4881b026f6dc555da759abcb0d9d1',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31856.html#a58c30d7e5be4257cd17f86aaccde80a9',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs)']]],
+  ['adafruit_5fmax31856',['Adafruit_MAX31856',['../class_adafruit___m_a_x31856.html',1,'Adafruit_MAX31856'],['../class_adafruit___m_a_x31856.html#a14a4881b026f6dc555da759abcb0d9d1',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31856.html#a1a83e5349a8dea6815f3cd82628801d5',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, SPIClass *_spi=&amp;SPI)']]],
   ['adafruit_5fmax31856_2ecpp',['Adafruit_MAX31856.cpp',['../_adafruit___m_a_x31856_8cpp.html',1,'']]],
   ['adafruit_5fmax31856_2eh',['Adafruit_MAX31856.h',['../_adafruit___m_a_x31856_8h.html',1,'']]],
   ['adafruit_20max31856_20thermocouple_20reader',['Adafruit MAX31856 thermocouple reader',['../index.html',1,'']]]
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fmax31856',['Adafruit_MAX31856',['../class_adafruit___m_a_x31856.html#a14a4881b026f6dc555da759abcb0d9d1',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31856.html#a58c30d7e5be4257cd17f86aaccde80a9',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs)']]]
+  ['adafruit_5fmax31856',['Adafruit_MAX31856',['../class_adafruit___m_a_x31856.html#a14a4881b026f6dc555da759abcb0d9d1',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, int8_t spi_mosi, int8_t spi_miso, int8_t spi_clk)'],['../class_adafruit___m_a_x31856.html#a1a83e5349a8dea6815f3cd82628801d5',1,'Adafruit_MAX31856::Adafruit_MAX31856(int8_t spi_cs, SPIClass *_spi=&amp;SPI)']]]
 ];
